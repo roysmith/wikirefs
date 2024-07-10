988791e8efea21064c9a87a1f24aa8b10eaf2bb1
Roy Smith <roy@panix.com>
2024-07-10 19:40:43 -0400

    Add wikirefs.get_statements()

--- tests/test_wikirefs.py
+++ tests/test_wikirefs.py
@@ -1,32 +1,93 @@
-from pathlib import Path
-
 import pytest
 from bs4 import BeautifulSoup
 
-import wikirefs
-
-
-@pytest.fixture
-def ref_sample_1():
-    with Path(__file__).parent / "ref-sample-1.html" as datafile:
-        return datafile.read_text()
+from wikirefs import get_statements, Citation, Statement
 
 
 def parse(text):
     return BeautifulSoup(text, features="lxml")
 
 
-class TestParseParagraph:
+class TestCitation:
+    @pytest.mark.parametrize(
+        "id_string, expected",
+        [
+            ("cite_ref-1", Citation("cite_ref-1", "1")),
+            ("cite_ref-:foo_1-0", Citation("cite_ref-:foo_1-0", "1", "foo", "0")),
+            ("cite_ref-:foo_1-2", Citation("cite_ref-:foo_1-2", "1", "foo", "2")),
+            ("cite_ref-:0_1-2", Citation("cite_ref-:0_1-2", "1", "0", "2")),
+        ],
+    )
+    def test_from_id(self, id_string, expected):
+        citation = Citation.from_id(id_string)
+        assert citation == expected
+
+    @pytest.mark.parametrize(
+        "id_string",
+        [
+            (""),
+            ("1"),
+            ("foo"),
+        ],
+    )
+    def test_from_id_parse_failure(self, id_string):
+        with pytest.raises(ValueError, match="cannot parse ref_id"):
+            Citation.from_id(id_string)
+
+    @pytest.mark.parametrize(
+        "citation, expected",
+        [
+            (Citation("", "", suffix=None), ""),
+            (Citation("", "", suffix="0"), "a"),
+            (Citation("", "", suffix="1"), "b"),
+            (Citation("", "", suffix="25"), "z"),
+            (Citation("", "", suffix="26"), "aa"),
+            (Citation("", "", suffix="27"), "ab"),
+            (Citation("", "", suffix="52"), "ba"),
+            (Citation("", "", suffix="53"), "bb"),
+        ],
+    )
+    def test_rendered_suffix(self, citation, expected):
+        rendered = citation.rendered_suffix()
+        assert rendered == expected
+
+    @pytest.mark.parametrize(
+        "n, expected_string",
+        [
+            (1, "a"),
+            (2, "b"),
+            (26, "z"),
+            (27, "aa"),
+        ],
+    )
+    def test_bijectiveHexavigesimal(self, n, expected_string):
+        assert Citation.bijectiveHexavigesimal(n) == expected_string
+
+
+class TestGetStatements:
     def test_no_text(self):
         p = parse("<p></p>")
-        assert wikirefs.parse_paragraph(p) == []
+        assert list(get_statements(p)) == []
 
     def test_one_statement(self):
         p = parse(
-            """<p>statement.
-            <sup id="cite_ref-1" class="reference">
-            <a href="#cite_note-1">&#91;1&#93;</a>
-            </sup> </p>"""
+            """
+            <p>statement.
+                <sup id="cite_ref-1" class="reference"><a href="#cite_note-1">&#91;1&#93;</a></sup>
+            </p>
+            """
         )
-        statements = wikirefs.parse_paragraph(p)
-        assert statements == [wikirefs.Statement("statement.", ["1"])]
+        statements = list(get_statements(p))
+        assert statements == [Statement("statement.", [Citation("cite_ref-1", "1")])]
+
+    def test_two_statements(self):
+        p = parse(
+            """
+            <p>
+                Statement
+                <sup id="cite_ref-1" class="reference"><a href="#cite_note-1">&#91;1&#93;</a></sup>
+                <sup id="cite_ref-2" class="reference"><a href="#cite_note-2">&#91;1&#93;</a></sup>
+            </p>
+        """
+        )
+        statements = list(get_statements(p))
--- wikirefs/__init__.py
+++ wikirefs/__init__.py
@@ -1,23 +1,121 @@
 from dataclasses import dataclass
+from enum import Enum
 import re
+from typing import Optional
+
+from bs4 import NavigableString, Tag
+
+import more_itertools
+
+
+@dataclass(frozen=True)
+class Citation:
+    """ref_id is the raw HTML "id" from the <sup> element.
+
+    number is user-visible citation number which goes inside the []
+
+    name is the reference name, either a name attribute supplied
+        in a <ref> element, or one invented by VisualEditor.
+    """
+
+    ref_id: str
+    number: str
+    name: str = None
+    suffix: str = None
+
+    @staticmethod
+    def from_id(ref_id: str):
+        pattern = re.compile(
+            r"""
+            cite_ref-               # constant prefix
+            (:(?P<name>[^_]+)_)?    # optional ref name (":0_" or ":foo_")
+            (?P<number>\d+)         # user-visible ref number ("1")
+            (-(?P<suffix>\d+))?     # optional ref number suffix ("-0")
+            """,
+            re.VERBOSE,
+        )
+        if m := re.fullmatch(pattern, ref_id):
+            return Citation(ref_id, **m.groupdict())
+        else:
+            raise ValueError(f"cannot parse ref_id '{ref_id}'")
+
+    def rendered_suffix(self):
+        if self.suffix:
+            return self.bijectiveHexavigesimal(int(self.suffix) + 1)
+        else:
+            return ""
+
+    # Courtesy of User:Ahecht (https://w.wiki/AcQC)
+    @staticmethod
+    def bijectiveHexavigesimal(n: int):
+        "Convert a integer (1-based) to bijective base-26"
+        outStr = ""
+        while n != 0:
+            outStr = chr((n - 1) % 26 + 97) + outStr
+            n = (n - 1) // 26
+        return outStr
 
 
 @dataclass(frozen=True)
 class Statement:
     text: str
-    citations: list[str]
+    citations: list[Citation]
 
 
-def parse_paragraph(paragraph):
-    text = re.sub(r"\s+", " ", paragraph.text).strip()
-    sups = paragraph.find_all("sup", class_="reference")
-    statements = []
-    working_text = text
-    for sup in sups:
-        ref_id = sup.get("id")
-        ref_label = ref_id.removeprefix("cite_ref-")
-        delim = f"[{ref_label}]"
-        text, more = working_text.split(delim, maxsplit=1)
-        statements.append(Statement(text.strip(), [ref_label]))
-        working_text = more
-    return statements
+class State(Enum):
+    """The last thing we've seen.  There is no explicit start state;
+    we start in STRING, which effectively means that last thing we've
+    seen is an empty string.
+    """
+
+    STRING = 1
+    CITATION = 2
+
+
+@staticmethod
+def citation_id(node: Tag) -> Optional[str]:
+    """If this is a citation marker, return the id of the enclosing
+    <sup> tag.  Otherwise, return None.
+
+    """
+    if isinstance(node, NavigableString) and node.parent.name == "a":
+        node2 = node.parent.parent
+        return (
+            node2.name == "sup"
+            and node2.get("class") == ["reference"]
+            and node2.get("id")
+        )
+
+
+def get_statements(p: Tag):
+    words = []
+    citations = []
+    state = State.STRING
+    for node in p.descendants:
+        if not isinstance(node, NavigableString):
+            continue
+        if node.string.strip() == "":
+            continue
+        match state:
+            case State.STRING:
+                if cid := citation_id(node):
+                    citations.append(Citation.from_id(cid))
+                    state = State.CITATION
+                else:
+                    words.extend(node.string.split())
+            case State.CITATION:
+                if cid := citation_id(node):
+                    citations.append(node.string)
+                else:
+                    text = " ".join(words)
+                    yield Statement(text, citations)
+                    words = node.string.split()
+                    citations = []
+            case _:
+                raise RuntimeError(f"Unknown state: {state}")
+
+    # We're reached the end of the paragraph, but we might need
+    # to flush the last statement collected.
+    if words or citations:
+        text = " ".join(words)
+        yield Statement(text, citations)
