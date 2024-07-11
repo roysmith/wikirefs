d9aea5bf6d7a5116dd9014b657ee0303520cf44e
Roy Smith <roy@panix.com>
2024-07-10 22:10:59 -0400

    Save permalink as HTML comment

--- tools/getHTML.py
+++ tools/getHTML.py
@@ -2,6 +2,6 @@ from pywikibot import Site, Page
 
 site = Site("en")
 page = Page(site, "User:RoySmith/sandbox")
-print(page)
+print(f"<!-- Created from {page.create_short_link(permalink=True)} -->")
 html = page.get_parsed_page()
 print(html)
