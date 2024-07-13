8085ef1d79179cf42cfec5e1518ffb42b601144a
Roy Smith <roy@panix.com>
2024-07-12 22:26:16 -0400

    Add primordial Flask app.

---
+++ web/webapp.py
@@ -0,0 +1,28 @@
+import secrets
+
+import flask
+from flask import render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+app = flask.Flask(__name__)
+app.config["SECRET_KEY"] = secrets.token_hex()
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    form = PageForm(request.form)
+    if request.method == "POST" and form.validate():
+        page_title = form.page_title.data
+        return redirect(url_for("show", page_title=page_title))
+    return render_template("page_form.html", form=form)
+
+
+@app.route("/show")
+def show():
+    return render_template("show.html", page_title=request.args.get("page_title"))
+
+
+class PageForm(FlaskForm):
+    page_title = StringField("page_title", validators=[DataRequired()])
