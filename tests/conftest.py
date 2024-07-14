aa0352ef461667e66bead2f627ddb126627fa33f
Roy Smith <roy@panix.com>
2024-07-14 17:52:34 -0400

    Add HTML sample containing non-repeated ref

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,9 @@ import pytest
 def sample_1_html():
     sample = Path(__file__).parent / "data/sample-1.html"
     return sample.read_text()
+
+
+@pytest.fixture
+def sample_2_html():
+    sample = Path(__file__).parent / "data/sample-2.html"
+    return sample.read_text()
